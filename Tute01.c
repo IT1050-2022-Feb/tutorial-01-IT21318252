e895bb4f79ca6860a35776a5fc9c8a40c11f80e0
Ammar <99312045+IT21318252@users.noreply.github.com>
2022-02-09 06:24:59 +0000

    completed

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,17 @@
 #include <stdio.h>
 
 int main() {
+   int mark , total = 0 ,x , average;
+
+  for(x=1 ; x<3 ; x++)
+    {
+      printf("Enter the mark %d : ",x);
+      scanf("%d", &mark);
+
+      total = total + mark;
+    }
+  average= total/2;
+  printf("Average mark : %d", average);
   
   return 0;
 }
